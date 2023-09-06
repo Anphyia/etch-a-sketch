9c7284c8517ba999690c3044c5f150ef8a16540b
Anphyia <anilberkanuzrek@hotmail.com>
2023-09-06 14:54:24 +0300

    feat(dynamic-sizing): add dynamic grid size calculation

---
+++ script.js
@@ -0,0 +1,22 @@
+
+function makeDiv(num) {
+    const container = document.querySelector('.container');
+
+    for (let i = 0; i < (num * num); i++) {
+        let cell = document.createElement('div');
+        container.appendChild(cell);
+    }
+
+const containerWidth = 960;
+const cellSize = containerWidth / num;
+
+container.style.width = `${containerWidth}px`;
+container.style.height = `${containerWidth}px`;
+
+const cells = container.querySelectorAll('div');
+cells.forEach(div => {
+    div.style.width = `${cellSize}px`;
+    div.style.height = `${cellSize}px`;
+});
+}
+makeDiv(16);
