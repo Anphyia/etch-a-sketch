924d98bfb867b6361472dc45f7c13300eda4c2ed
Anphyia <anilberkanuzrek@hotmail.com>
2023-09-08 22:46:50 +0300

    Improve grid resizing functionality via a button

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const container = document.querySelector('.container');
+const resizeButton = document.getElementById('resizeButton');
 let cells;
 
 function makeDiv(num) {
@@ -7,7 +8,7 @@ function makeDiv(num) {
         container.appendChild(cell);
     }
 
-    const containerWidth = 960;
+    const containerWidth = 720;
     const cellSize = containerWidth / num;
 
     container.style.width = `${containerWidth}px`;
@@ -18,6 +19,8 @@ function makeDiv(num) {
         div.style.width = `${cellSize}px`;
         div.style.height = `${cellSize}px`;
     });
+
+    colorDiv();
 }
 
 function colorDiv() {
@@ -28,6 +31,31 @@ function colorDiv() {
     });    
 }
 
-makeDiv(32);
-colorDiv();
+function setDivSize() {
+    let cellNumber = prompt("Enter the number of squares per side (Max: 100)", 32);
+    cellNumber = parseInt(cellNumber, 10);
 
+    if (isNaN(cellNumber)) {
+        alert("You must enter a valid integer.");
+        return;
+    } else if (cellNumber > 100) {
+        alert("You must enter a number less than 100.");
+        return;
+    } else if (cellNumber <= 0) {
+        alert("You must enter a positive integer.");
+        return;
+    } else {
+        while (container.firstChild) {
+            container.removeChild(container.firstChild);
+        }
+
+        makeDiv(cellNumber);
+    }
+}
+
+
+
+makeDiv(32);
+resizeButton.addEventListener('click', () => {
+    setDivSize();
+});
